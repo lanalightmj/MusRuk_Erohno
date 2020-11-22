570ab01c39de97b28fcf366f6249776a6fa5974c
lanalightmj <lanalightmj@i.ua>
2020-11-22 23:27:27 +0200

    for mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-$('.carousel').carousel()
+//carousel
+$('.carousel').carousel();
+
+//marquee
+$('.marquee').marquee({
+	//duration in milliseconds of the marquee
+	duration: 15000,
+	//gap in pixels between the tickers
+	gap: 70,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true
+});
